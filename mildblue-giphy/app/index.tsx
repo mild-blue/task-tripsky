7163bc20ecf91bbf6541c100019c009e64794a72
Tr1po <andrej@soundsgood.agency>
2025-12-08 22:11:27 +0100

    minimal working folder

---
+++ mildblue-giphy/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+	return (
+		<View>
+			<Text style={{ fontSize: 20 }}>Minimal Expo Router Project</Text>
+		</View>
+	);
+}
